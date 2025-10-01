7630d895feb139dabb34f078d476da9b6d1a58da
Renan Rosas <renan-rosas@hotmail.com>
2025-09-30 21:04:06 -0300

    Reset global.css, theme.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/theme.css';
+import './styles/global.css';
+import { Heading } from './components/Heading';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <Heading />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis quam
+        atque molestias fuga quisquam tenetur! Necessitatibus quod, hic illum
+        minus harum ut molestias fugiat nisi quis temporibus minima adipisci
+        enim.
       </p>
     </>
-  )
+  );
 }
-
-export default App
