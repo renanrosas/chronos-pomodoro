8661962340eeee5917ebae90e6cfa5874d89df1f
Renan Rosas <renan-rosas@hotmail.com>
2025-10-02 08:58:03 -0300

    Heading component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './styles/theme.css';
 import './styles/global.css';
 import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading />
+      <Heading>
+        Olá mundo!
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis quam
         atque molestias fuga quisquam tenetur! Necessitatibus quod, hic illum
