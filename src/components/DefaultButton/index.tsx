45054641f06e8d22a912d316ed3546bcb618d6fd
Renan Rosas <renan-rosas@hotmail.com>
2025-10-03 21:41:11 -0300

    DefaultButton component

---
+++ src/components/DefaultButton/index.tsx
@@ -0,0 +1,19 @@
+import type React from 'react';
+import styles from './styles.module.css';
+
+type DefaultButtonProps = {
+  icon: React.ReactNode;
+  color?: 'green' | 'red';
+} & React.ComponentProps<'button'>;
+
+export function DefaultButton({
+  icon,
+  color = 'green',
+  ...props
+}: DefaultButtonProps) {
+  return (
+    <button className={`${styles.button} ${styles[color]}`} {...props}>
+      {icon}
+    </button>
+  );
+}
