6d67a734d18d88f1b00f40e6c4d0fb5b6f8dde5c
Renan Rosas <renan-rosas@hotmail.com>
2025-10-03 21:56:18 -0300

    Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 export function App() {
   return (
@@ -49,6 +50,10 @@ export function App() {
             <DefaultButton icon={<StopCircleIcon />} color='red' />
           </div>
         </form>
+      </Container>
+
+      <Container>
+        <Footer />
       </Container>
     </>
   );
