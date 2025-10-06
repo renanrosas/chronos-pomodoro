e91701e2885f70f0ad1715b69b01f5dc71d18556
Renan Rosas <renan-rosas@hotmail.com>
2025-10-05 22:13:12 -0300

    Home page

---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,32 @@
+import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
+import { Cycles } from '../Cycles';
+import { DefaultButton } from '../DefaultButton';
+import { DefaultInput } from '../DefaultInput';
+
+export function MainForm() {
+  return (
+    <form className='form' action=''>
+      <div className='formRow'>
+        <DefaultInput
+          labelText='Task'
+          id='input'
+          type='text'
+          placeholder='Digite algo'
+        />
+      </div>
+
+      <div className='formRow'>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+
+      <div className='formRow'>
+        <Cycles />
+      </div>
+
+      <div className='formRow'>
+        <DefaultButton icon={<PlayCircleIcon />} color='green' />
+        <DefaultButton icon={<StopCircleIcon />} color='red' />
+      </div>
+    </form>
+  );
+}
