ba7585e7082a14dc25ec2559ab692bf0169fd102
Renan Rosas <renan-rosas@hotmail.com>
2025-10-07 21:33:58 -0300

    TaskContext, context API config

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import './styles/theme.css';
 import './styles/global.css';
 
 import { Home } from './pages/Home';
+import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 
 export function App() {
-  return <Home />;
+  return (
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
+  );
 }
